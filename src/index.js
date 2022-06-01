6a373aa92aa3ebd6b405d8066a99bc4170f0d1d0
marrodri <laza.mar64@gmail.com>
2022-06-01 14:54:28 -0700

    chore: status text fixed

--- src/index.js
+++ src/index.js
@@ -128,7 +128,7 @@ class Game extends React.Component {
 					/>
 				</div>
 				<div className='game-info'>
-					<div>status</div>
+					<div>{status}</div>
 					<ol>{moves}</ol>
 				</div>
 			</div>
