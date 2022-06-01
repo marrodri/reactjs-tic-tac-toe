f6be03d702cf2e70d97819e1f88426a1dc2d3094
marrodri <laza.mar64@gmail.com>
2022-05-31 21:58:31 -0700

    code comments cleanup

--- src/index.js
+++ src/index.js
@@ -5,26 +5,8 @@ import './index.css';
 
 
 class SquareDefunct extends React.Component {
-	//setting and initializing the state
-	//super is needed for defining the constructor of a subclass.
-	// now its useless, I just have it for reference. 
-	// constructor(props) {
-	// 	super(props);
-	// 	this.state = {
-	// 		value: null,
-	// 	};
-	// }
-
 	render() {
 		return (
-			// the square is a class already defined from the framework,
-			// without it, it shows small circular buttons. The props of this
-			// class, are passed with the renderSquare method, at the Board class.
-			// the props name are defined from its parent class; 'value' name
-			//and value is named and defined in the Board class.
-			// the => function, will make it as a single use function.
-			//not using the =>, will run automatic the instructions inside the
-			// bracket.
 			<button className="square"
 				onClick={() => this.props.onClick()}
 			>
@@ -34,13 +16,8 @@ class SquareDefunct extends React.Component {
 	}
 }
 
-// function component. 
-// this is useful if an object is rendered and doesnt
-// have its own state.
 function Square(props) {
 	return (<button className="square"
-		//the ()=> is dismissed because now this is a function as a whole
-		//and not a class waiting for a click
 		onClick={props.onClick
 		}>
 		{props.value}
@@ -48,30 +25,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-
-
-	//
-	// handleClick(i) {
-	// 	const squares = this.state.squares.slice();
-	// 	if(calculateWinner(squares) || squares[i]){
-	// 		return ;
-	// 	}
-	// 	squares[i] = this.state.xIsNext ? 'X' : 'O';
-	// 	this.setState({
-	// 		squares: squares,
-	// 		xIsNext: !this.state.xIsNext,
-	// });
-	// }
-
-	// the i is a property(prop) for the Square class.
-	//it seems that a method is needed for rendering an html element.
-	//TODO: question. check later with ben, if this is mandatory.
-
-
-	// props to pass
-	// squares[i] = 'x';
-	// index of the square.
-	// value to pass.
 	renderSquare(i) {
 		return (
 			<Square
@@ -79,18 +32,12 @@ class Board extends React.Component {
 				onClick={() => this.props.onClick(i)}
 			/>
 		);
-		// the code below can be used as a decor UI element.
-		// return <Square />;
 	}
 
 	render() {
 		return (
 			<div>
 				<div className="board-row">
-					{/* keyword 'this.' helps to select and run the methods
-					that are defined in this same class, check later
-					if its possible, to use methods from other 
-					component classes. */}
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
 					{this.renderSquare(2)}
@@ -190,17 +137,13 @@ class Game extends React.Component {
 
 }
 
-// other functions =================
-
-
-
-
 // ==============================
 
 // TODO: what is createRoot the same as the runApp from flutter.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
 
+// other functions =================
 
 function calculateWinner(squares) {
 	const lines = [
