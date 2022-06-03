1275ba1bacf4c2860ac0cc32e02db8663cd83842
marrodri <laza.mar64@gmail.com>
2022-06-03 08:15:25 -0700

    update: refactoring board class to function

--- src/index.js
+++ src/index.js
@@ -3,19 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
-
-class SquareDefunct extends React.Component {
-	render() {
-		return (
-			<button className="square"
-				onClick={() => this.props.onClick()}
-			>
-				{this.props.value}
-			</button>
-		);
-	}
-}
-
 function Square(props) {
 	return (<button className="square"
 		onClick={props.onClick
@@ -24,38 +11,75 @@ function Square(props) {
 	</button>);
 }
 
-class Board extends React.Component {
-	renderSquare(i) {
-		return (
-			<Square
-				value={this.props.squares[i]}
-				onClick={() => this.props.onClick(i)}
-			/>
-		);
-	}
+// renderSquare(i) {
+// 	return (
+// 		<Square
+// 			value={this.props.squares[i]}
+// 			onClick={() => this.props.onClick(i)}
+// 		/>
+// 	);
+// }
 
-	render() {
-		return (
-			<div>
-				<div className="board-row">
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
+function Board(props){
+	return (
+		<div>
+			<div className="board-row">
+				{/* {this.renderSquare(0)}
+				{this.renderSquare(1)}
+				{this.renderSquare(2)} */}
+
+				<Square
+					value={props.squares[0]}
+					onClick={() => props.onClick(0)}
+				/>
+				<Square
+					value={props.squares[1]}
+					onClick={() => props.onClick(1)}
+				/>
+				<Square
+					value={props.squares[2]}
+					onClick={() => props.onClick(2)}
+				/>
 			</div>
-		);
-	}
+
+			<div className="board-row">
+				{/* {this.renderSquare(3)}
+				{this.renderSquare(4)}
+				{this.renderSquare(5)} */}
+				<Square
+					value={props.squares[3]}
+					onClick={() => props.onClick(3)}
+				/>
+				<Square
+					value={props.squares[4]}
+					onClick={() => props.onClick(4)}
+				/>
+				<Square
+					value={props.squares[5]}
+					onClick={() => props.onClick(5)}
+				/>
+			</div>
+			<div className="board-row">
+				{/* {this.renderSquare(6)}
+				{this.renderSquare(7)}
+				{this.renderSquare(8)} */}
+				<Square
+					value={props.squares[6]}
+					onClick={() => props.onClick(6)}
+				/>
+				<Square
+					value={props.squares[7]}
+					onClick={() => props.onClick(7)}
+				/>
+				<Square
+					value={props.squares[8]}
+					onClick={() => props.onClick(8)}
+				/>
+			</div>
+		</div>
+	);
 }
+
 
 class Game extends React.Component {
 	constructor(props) {
