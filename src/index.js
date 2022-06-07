9763a29c522037ea8e50e3b8fc600cece338e7e9
marrodri <laza.mar64@gmail.com>
2022-06-06 21:37:11 -0700

    chore: comments added for notifying the bugs

--- src/index.js
+++ src/index.js
@@ -45,6 +45,7 @@ function Game(){
 	const [xIsNext, setXIsNext] = useState(true);
 
 	const handleClick = (i) => {
+		console.log('click No. '+ i);
 		const copyHistory = history.slice(0, stepNumber + 1);
 		const current = copyHistory[copyHistory.length - 1];
 		const squares = current.squares.slice();
@@ -59,36 +60,36 @@ function Game(){
 		setXIsNext(!xIsNext);
 	}
 
-	// const jumpTo = (step) => {
-	// 	setStepNumber(step);
-	// 	setXIsNext((step % 2) === 0);
-	// }
+	const jumpTo = (step) => {
+		setStepNumber(step);
+		setXIsNext((step % 2) === 0);
+	}
 
-	// TODO checkpoint. move the render logic to this part.
+	// There's a bug here, it's in a infinite loop
 	// const moves = history.map((step, move) => {
 	// 	const desc = move ?
 	// 	'Go to move #' + move : 'Go to game start';
 	// 	return (
 	// 		<li key={move}>
-	// 		<button onClick={()=>this.jumpTo(move)}>{desc}</button>
+	// 		<button onClick={()=>jumpTo(move)}>{desc}</button>
 	// 	</li>);
 	// });
 
-	let status = 'status';
-
-	// if(winner){
-	// 	status = 'Winner ' + winner;
-	// }
-	// else{
-	// 	status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-	// }
+	const winner = calculateWinner(history.squares);
+	let status;
+	if(winner){
+		status = 'Winner ' + winner;
+	}
+	else{
+		status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+	}
 
 	return(
-		
 		<div className='game'>
 			<div>
 				<Board 
 					squares={history.squares}
+					// for some reason, it doesn't react when clicking.
 					onClick={(i) => handleClick(i)}
 				/>
 			</div>
