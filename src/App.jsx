87ad70e07e398d109cb232c1cfe344b158ef0233
marrodri <laza.mar64@gmail.com>
2022-06-10 12:05:36 -0700

    chore: basic functioning navigational route added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { Outlet, Link } from "react-router-dom";
 export default function App() {
   return (
     <div>
+	<h1>Tic Tac Toe, Please click on Game to start a game</h1>
       <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
         }}
       >
-		<Link to='/index'>index</Link>
+		<Link to='/index'>index</Link> {"| "}
 		<Link to='/aboutThis'>About This</Link>
 	  </nav>
 	  <Outlet/>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
+// dependencies
+import React from "react";
 import ReactDOM from "react-dom/client";
-import Game from "./routes/game"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// function components
+import App from "./App";
+import Game from "./routes/game";
+import AboutThis from "./routes/aboutThis";
 
 // ==============================
 
 // TODO: what is createRoot the same as the runApp from flutter.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      {/* nest these routes inside App */}
+      <Route path="game" element={<Game />} />
+      <Route path="aboutThis" element={<AboutThis />} />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>404: there's nothing here.</p>
+          </main>
+        }
+      />
+    </Routes>
+    <Game />
+  </BrowserRouter>
+);
 
 // other functions =================
-
