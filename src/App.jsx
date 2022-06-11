cc6359b173ae0ad3abdc04b943a76fd8c2dcd2dc
marrodri <laza.mar64@gmail.com>
2022-06-11 12:43:49 -0700

    chore: basic tailwindcss added

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,37 @@ import { Outlet, Link } from "react-router-dom";
 export default function App() {
   return (
     <div>
-	<h1>Tic Tac Toe, Please click on Game to start a game</h1>
+      <h1 class="
+      font-medium 
+      leading-tight 
+       text-5xl 
+      mt-0
+       mb-2
+      text-blue-600">
+        Tic Tac Toe
+      </h1>
+      <h2 class="
+        font-medium
+        leading-tight
+        text-4xl
+        mt-0
+        mb-2
+        text-blue-600
+      ">
+         Please click on Game to start a game
+
+      </h2>
       <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
         }}
       >
-		<Link to='/Game'>Game</Link> {"| "}
-		<Link to='/aboutThis'>About This</Link> {"| "}
-		<Link to='/resources'>Resources</Link>
-	  </nav>
-	  <Outlet/>
+        <Link className="text-2xl" to="/Game">Game</Link> {"| "}
+        <Link className="text-2xl" to="/aboutThis">About This</Link> {"| "}
+        <Link className="text-2xl" to="/resources">Resources</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
--- src/routes/aboutThis.jsx
+++ src/routes/aboutThis.jsx
@@ -1,12 +1,25 @@
 // TODO
 export default function AboutThis() {
-	return (<main style={{padding: "1rem 0"}}>
-		<h2>About This</h2>
-		<p>
-			This website is built with the purpose of {" \n"}
-			learning the basics and foundations of reactJS; along with website {" \n"}
-			deployment with Netlify, navigation with react router dom, {" \n"}
-			and styling with TailWind. 
-		</p>
-	</main>)
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h3
+        class="font-medium
+					leading-tight
+					text-3xl
+					mt-0
+					mb-2
+					text-blue-500
+				"
+      >
+        About This
+      </h3>
+      <p className="text-2xl">
+        This website is built with the purpose of {" \n"}
+        learning the basics and foundations of reactJS; along with website{" "}
+        {" \n"}
+        deployment with Netlify, navigation with react router dom, {" \n"}
+        and styling with TailWind.
+      </p>
+    </main>
+  );
 }
